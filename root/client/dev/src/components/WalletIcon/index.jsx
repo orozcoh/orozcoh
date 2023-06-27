e721c041e82ebba957da46618300ab2c878ad674
Santiago Orozco <dev.orozcoh@gmail.com>
2023-06-27 16:51:37 -0500

    FE-002: Print keplr addr to console

--- root/client/dev/src/components/WalletIcon/index.jsx
+++ root/client/dev/src/components/WalletIcon/index.jsx
@@ -7,32 +7,22 @@ export const WalletIcon = () => {
       alert("Please install Keplr extension");
     } else {
       const chainId = "cosmoshub-4";
-
-      // Enabling before using Keplr is recommended.
-      // This method will ask the user whether to allow access if they haven't visited this website.
-      // Also, it will request that the user unlock the wallet if the wallet is locked.
       await window.keplr.enable(chainId);
 
       const offlineSigner = window.keplr.getOfflineSigner(chainId);
-      // You can get the address/public keys by `getAccounts` method.
-      // It can return the array of address/public key.
-      // But, currently, Keplr extension manages only one address/public key pair.
+
+      // Get the accounts
       const accounts = await offlineSigner.getAccounts();
-      //const address = accounts[0].address;
+      const address = accounts[0].address;
+      console.log("Wallet Address:", address);
 
-      console.log("Wallet Address:", accounts);
-
-      // Verify the signature
-      /*       const isValid = await offlineSigner.verifySignature(
-        signedMessage.signature,
-        message,
-        publicKey
+      /*       const message = "Hello, Keplr!";
+      const signature = await window.keplr.signArbitrary(
+        chainId,
+        address,
+        message
       );
-
-      console.log("Signature Verification:", isValid); */
-
-      // Use the verification result or perform other operations
-      // ...
+      console.log("sig:", signature); */
     }
   };
 
