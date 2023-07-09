d0f1fc758f2345a00c0475438010826bf5805117
Santiago Orozco <dev.orozcoh@gmail.com>
2023-07-09 18:25:58 -0500

    BE-016: change path

--- root/client/dev/src/global/var/index.js
+++ root/client/dev/src/global/var/index.js
@@ -1,6 +1,6 @@
 const envVars = {
-  //API2_URL: "api2.orozcoh.com", // Cloud
-  API2_URL: "192.168.1.2:3000", // Local
+  API2_URL: "api2.orozcoh.com", // Cloud
+  //API2_URL: "192.168.1.2:3000", // Local
   //API2_URL: "192.168.1.200:3000",   // Raps
 };
 
